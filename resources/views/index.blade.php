43d07398a71963b26578b9b811e068b4b42e5453
Jacob <jacob.atam@gmail.com>
2024-08-05 22:31:29 +0100

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -146,7 +146,7 @@
 											<strong class="text-4-5"><a href="tel:{{$system_settings->phone}}" class="text-color-light text-decoration-none">{{$system_settings->phone}}</a></strong>
 										</div>
 									</div>
-									<a href="/book-appointment" class="btn btn-light btn-outline custom-btn-border-radius font-weight-bold text-color-light text-color-hover-dark bg-color-hover-light btn-px-5 btn-py-3">BOOK AN APPOINTMENT</a>
+									<a href="/book-appointment" class="btn btn-light btn-outline custom-btn-border-radius font-weight-bold text-color-light text-color-hover-dark bg-color-hover-light btn-px-5 btn-py-3">Get a Quote</a>
 								</div>
 							</div>
 						</div>
