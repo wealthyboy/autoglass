7dc92ffd4e06f4501b16d652f6f9f1f01daadee0
Jacob <jacob.atam@gmail.com>
2024-08-22 14:08:30 +0100

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,12 @@
 	<div class="position-absolute top-0 left-0 right-0 bottom-0 ">
 		<div class="background-image-wrapper custom-background-style-1 position-absolute top-0 left-0 right-0 bottom-0 " style="background-image: url(/images/banners/i3.jpg); background-repeat: no-repeat; background-size: cover;"></div>
 	</div>
-	<div class="container position-relative py-sm-5 my-5">
+	
+	<div class="container position-relative py-sm-5">
+		<div class="d-block d-md-none mb-3  position-relative  right-77">
+		    <img src="/images/banners/mobeil_banner_autoglass.jpg" alt="autoglass">
+	   </div>
+
 		<svg class="custom-svg-1 d-none d-sm-block" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 649 578">
 			<path fill="#FFF" d="M-225.5,154.7l358.45,456.96c7.71,9.83,21.92,11.54,31.75,3.84l456.96-358.45c9.83-7.71,11.54-21.92,3.84-31.75
 				L267.05-231.66c-7.71-9.83-21.92-11.54-31.75-3.84l-456.96,358.45C-231.49,130.66-233.2,144.87-225.5,154.7z"/>
@@ -36,130 +41,130 @@
 
 				
 
-				<div class="container my-5 pt-md-4 pt-xl-0">
-					<div class="row align-items-center justify-content-center pb-4 mb-5">
-						 <!-- First column that should appear first on mobile -->
-						<div class="col-12 col-md-9 col-lg-6 ps-lg-5 order-1 order-md-2 "
+		<div class="container my-5 pt-md-4 pt-xl-0">
+			<div class="row align-items-center justify-content-center pb-4 mb-5">
+					<!-- First column that should appear first on mobile -->
+				<div class="col-12 col-md-9 col-lg-6 ps-lg-5 order-1 order-md-2 "
+					>
+				<div class="position-relative">
+					<div class="image-container">
+					<img src="/images/banners/repair_glass_autoglassng.jpg" class="img-fluid rounded mb-3 " alt="" />
+					</div>
+				</div>
+				</div>
+
+				<!-- Second column that should appear second on mobile -->
+				<div class="col-lg-6 pb-sm-4 pb-lg-0 mb-5 mb-lg-0 order-2 order-md-1">
+					<div class="overflow-hidden">
+						<h2 class="font-weight-bold text-color-dark line-height-1 mb-0 "
+							>5 Years of Top Experience</h2>
+					</div>
+					<div class="custom-divider divider divider-primary divider-small my-3">
+						<hr class="my-0 "
 							>
-						<div class="position-relative">
-							<div class="image-container">
-							<img src="/images/banners/repair_glass_autoglassng.jpg" class="img-fluid rounded mb-3 " alt="" />
-							</div>
-						</div>
-						</div>
-
-						<!-- Second column that should appear second on mobile -->
-						<div class="col-lg-6 pb-sm-4 pb-lg-0 mb-5 mb-lg-0 order-2 order-md-1">
-							<div class="overflow-hidden">
-								<h2 class="font-weight-bold text-color-dark line-height-1 mb-0 "
-									>5 Years of Top Experience</h2>
-							</div>
-							<div class="custom-divider divider divider-primary divider-small my-3">
-								<hr class="my-0 "
-									>
-							</div>
-						<p class="font-weight-light text-3-5 mb-4 "
-							>Over the past five years, We have dedicated ourselves to mastering the intricacies of the auto glass industry, gaining a wealth of experience that spans from hands-on technical skills to comprehensive business management.</p>
-						<p class="pb-1 mb-4 "
-						>Our journey in this field has been marked by continuous learning, excellence customer service, and adapting to technological advancements.</p>
-						<div class="d-flex align-items-start align-items-sm-center flex-column flex-sm-row">
-							<a href="/pages/about-us" class="btn btn-primary custom-btn-border-radius font-weight-bold text-3 px-5 btn-py-3 me-sm-2 mb-3 mb-sm-0 "
-							>VIEW MORE</a>
-							<div class="feature-box align-items-center border border-top-0 border-end-0 border-bottom-0 custom-remove-mobile-xs-border-left ms-sm-4 ps-sm-4 "
-								data-appear-animation="fadeInRightShorterPlus"
-								data-appear-animation-delay="1200">
-							<div class="feature-box-icon bg-transparent">
-								<i class="icons icon-phone text-6 text-color-dark"></i>
-							</div>
-							<div class="feature-box-info line-height-2 ps-1">
-								<span class="d-block text-1 font-weight-semibold text-color-default">CALL US NOW</span>
-								<strong class="text-4-5"><a href="tel:{{$system_settings->phone}}" class="text-color-dark text-color-hover-primary text-decoration-none">{{$system_settings->phone}}</a></strong>
-							</div>
-							</div>
-						</div>
-						</div>
 					</div>
-				
-				</div>
-				<section class="section section-height-3 bg-grey  border-0 m-0">
-
-
-				<div class="container my-5 pt-md-4 pt-xl-0">
-					
-					<div class="row pb-2">
-						<div class="col-lg-4 text-center px-lg-5 mb-5 mb-lg-0">
-							<a  class="text-decoration-none">
-								<div class="custom-icon-box-style-1 ">
-									<div class="custom-icon-style-1 mb-4">
-										<svg xmlns="http://www.w3.org/2000/svg"   width="40" height="40" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
-											<path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
-										</svg>
-									</div>
-									<h3 class="text-transform-none font-weight-bold text-color-dark line-height-3 text-4-5 px-3 px-xl-5 my-2">Mobile Services</h3>
-									<p>Our technicians come to your home, workplace, or any preferred location, saving you the time and trouble of visiting a shop. </p>
-								</div>
-							</a>
-						</div>
-						<div class="col-lg-4 text-center px-lg-5 mb-5 mb-lg-0">
-							<a  class="text-decoration-none">
-								<div class="custom-icon-box-style-1 ">
-									<div class="custom-icon-style-1 mb-4">
-									<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-shield-check" viewBox="0 0 16 16">
-											<path d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56"/>
-											<path d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
-									</svg>
-									</div>
-									<h3 class="text-transform-none font-weight-bold text-color-dark line-height-3 text-4-5 px-xl-5 my-2 mx-4">Quality</h3>
-									<p>We only use premium materials such as OEM or its Equivalent and Advanced Adhesives to ensure Durability and Fit.</p>
-								</div>
-							</a>
-						</div>
-						<div class="col-lg-4 text-center px-lg-5">
-							<a  class="text-decoration-none">
-								<div class="custom-icon-box-style-1 ">
-									<div class="custom-icon-style-1 mb-4">
-									    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
-										  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
-										</svg>									
-									</div>
-									<h3 class="text-transform-none font-weight-bold text-color-dark line-height-3 text-4-5 px-4 px-xl-5 my-2">Professionalism  <i class="bi bi-airplane-engines"></i></h3>
-									<p>Our technicians are highly trained and certified, ensuring expert installation and repairs.</p>
-								</div>
-							</a>
-						</div>
+				<p class="font-weight-light text-3-5 mb-4 "
+					>Over the past five years, We have dedicated ourselves to mastering the intricacies of the auto glass industry, gaining a wealth of experience that spans from hands-on technical skills to comprehensive business management.</p>
+				<p class="pb-1 mb-4 "
+				>Our journey in this field has been marked by continuous learning, excellence customer service, and adapting to technological advancements.</p>
+				<div class="d-flex align-items-start align-items-sm-center flex-column flex-sm-row">
+					<a href="/pages/about-us" class="btn btn-primary custom-btn-border-radius font-weight-bold text-3 px-5 btn-py-3 me-sm-2 mb-3 mb-sm-0 "
+					>VIEW MORE</a>
+					<div class="feature-box align-items-center border border-top-0 border-end-0 border-bottom-0 custom-remove-mobile-xs-border-left ms-sm-4 ps-sm-4 "
+						data-appear-animation="fadeInRightShorterPlus"
+						data-appear-animation-delay="1200">
+					<div class="feature-box-icon bg-transparent">
+						<i class="icons icon-phone text-6 text-color-dark"></i>
+					</div>
+					<div class="feature-box-info line-height-2 ps-1">
+						<span class="d-block text-1 font-weight-semibold text-color-default">CALL US NOW</span>
+						<strong class="text-4-5"><a href="tel:{{$system_settings->phone}}" class="text-color-dark text-color-hover-primary text-decoration-none">{{$system_settings->phone}}</a></strong>
+					</div>
 					</div>
 				</div>
-				</section>
+				</div>
+			</div>
+		
+		</div>
+		<section class="section section-height-3 bg-grey  border-0 m-0">
 
+
+		<div class="container my-5 pt-md-4 pt-xl-0">
 			
-
-				<section class="section section-height-3 bg-primary border-0 m-0">
-					<div class="container">
-						<div class="row align-items-center">
-							<div class="col-lg-6 col-xl-7 text-center text-lg-start mb-4 mb-lg-0">
-								<h2 class="text-color-light font-weight-medium text-3-5 line-height-2 line-height-sm-1 ls-0 mb-2 mb-lg-2">Looking for Honest and Reliable Services?
-								</h2>
-								<h3 class="font-weight-bold text-color-light text-transform-none text-8 line-height-2 line-height-lg-1 mb-1">Best Auto Glass replacement and repair Experts in Lagos, Nigeria. 
-								</h3>
-								<p class="font-weight-bold text-color-light text-4 opacity-7 mb-0">Book An Appointment Today With Our Platform</p>
+			<div class="row pb-2">
+				<div class="col-lg-4 text-center px-lg-5 mb-5 mb-lg-0">
+					<a  class="text-decoration-none">
+						<div class="custom-icon-box-style-1 ">
+							<div class="custom-icon-style-1 mb-4">
+								<svg xmlns="http://www.w3.org/2000/svg"   width="40" height="40" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+									<path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
+								</svg>
 							</div>
-							<div class="col-lg-6 col-xl-5">
-								<div class="d-flex flex-column flex-lg-row align-items-center justify-content-between">
-									<div class="feature-box align-items-center mb-3 mb-lg-0">
-										<div class="feature-box-icon bg-transparent">
-											<i class="icons icon-phone text-6 text-color-light"></i>
-										</div>
-										<div class="feature-box-info line-height-2 ps-1">
-											<span class="d-block text-1 font-weight-semibold text-color-light mb-1">CALL US NOW</span>
-											<strong class="text-4-5"><a href="tel:{{$system_settings->phone}}" class="text-color-light text-decoration-none">{{$system_settings->phone}}</a></strong>
-										</div>
-									</div>
-									<a href="/book-appointment" class="btn btn-light btn-outline custom-btn-border-radius font-weight-bold text-color-light text-color-hover-dark bg-color-hover-light btn-px-5 btn-py-3">Get a Quote</a>
+							<h3 class="text-transform-none font-weight-bold text-color-dark line-height-3 text-4-5 px-3 px-xl-5 my-2">Mobile Services</h3>
+							<p>Our technicians come to your home, workplace, or any preferred location, saving you the time and trouble of visiting a shop. </p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 text-center px-lg-5 mb-5 mb-lg-0">
+					<a  class="text-decoration-none">
+						<div class="custom-icon-box-style-1 ">
+							<div class="custom-icon-style-1 mb-4">
+							<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-shield-check" viewBox="0 0 16 16">
+									<path d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56"/>
+									<path d="M10.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0"/>
+							</svg>
+							</div>
+							<h3 class="text-transform-none font-weight-bold text-color-dark line-height-3 text-4-5 px-xl-5 my-2 mx-4">Quality</h3>
+							<p>We only use premium materials such as OEM or its Equivalent and Advanced Adhesives to ensure Durability and Fit.</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-lg-4 text-center px-lg-5">
+					<a  class="text-decoration-none">
+						<div class="custom-icon-box-style-1 ">
+							<div class="custom-icon-style-1 mb-4">
+								<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+									<path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+								</svg>									
+							</div>
+							<h3 class="text-transform-none font-weight-bold text-color-dark line-height-3 text-4-5 px-4 px-xl-5 my-2">Professionalism  <i class="bi bi-airplane-engines"></i></h3>
+							<p>Our technicians are highly trained and certified, ensuring expert installation and repairs.</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+		</section>
+
+	
+
+		<section class="section section-height-3 bg-primary border-0 m-0">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-6 col-xl-7 text-center text-lg-start mb-4 mb-lg-0">
+						<h2 class="text-color-light font-weight-medium text-3-5 line-height-2 line-height-sm-1 ls-0 mb-2 mb-lg-2">Looking for Honest and Reliable Services?
+						</h2>
+						<h3 class="font-weight-bold text-color-light text-transform-none text-8 line-height-2 line-height-lg-1 mb-1">Best Auto Glass replacement and repair Experts in Lagos, Nigeria. 
+						</h3>
+						<p class="font-weight-bold text-color-light text-4 opacity-7 mb-0">Book An Appointment Today With Our Platform</p>
+					</div>
+					<div class="col-lg-6 col-xl-5">
+						<div class="d-flex flex-column flex-lg-row align-items-center justify-content-between">
+							<div class="feature-box align-items-center mb-3 mb-lg-0">
+								<div class="feature-box-icon bg-transparent">
+									<i class="icons icon-phone text-6 text-color-light"></i>
+								</div>
+								<div class="feature-box-info line-height-2 ps-1">
+									<span class="d-block text-1 font-weight-semibold text-color-light mb-1">CALL US NOW</span>
+									<strong class="text-4-5"><a href="tel:{{$system_settings->phone}}" class="text-color-light text-decoration-none">{{$system_settings->phone}}</a></strong>
 								</div>
 							</div>
+							<a href="/book-appointment" class="btn btn-light btn-outline custom-btn-border-radius font-weight-bold text-color-light text-color-hover-dark bg-color-hover-light btn-px-5 btn-py-3">Get a Quote</a>
 						</div>
 					</div>
-				</section>
+				</div>
+			</div>
+		</section>
 
 	
 
